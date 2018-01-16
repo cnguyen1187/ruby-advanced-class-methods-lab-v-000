3bc1e55cedc7190921d0dd60d1a54097bd20482b
Cam Nguyen <cnguyen1187@gmail.com>
2018-01-16 23:53:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,10 +10,22 @@ class Song
     self.class.all << self
   end
 
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create
+        song = self.new
+        @@all << song
+        song
+  end
+
   def self.create_by_name(name)
-      song = new_by_name(name)
-      song.save
-      song
+    song = new_by_name(name)
+    song.save
+    song
   end
 
   def self.find_by_name(name)
